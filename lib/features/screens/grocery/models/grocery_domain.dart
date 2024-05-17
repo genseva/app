1c7f959dbe8866f278edd9e5e2cd820700827437
bhawyaahuja <bhawyahuja@gmail.com>
2024-05-17 17:08:30 +0530

    update: grocery domain

---
+++ lib/features/screens/grocery/models/grocery_domain.dart
@@ -0,0 +1,8 @@
+class GroceryDomain{
+  final String name;
+  final String image;
+  final String quantity;
+  final double price;
+
+  GroceryDomain({required this.name, required this.image, required this.quantity, required this.price});
+}
