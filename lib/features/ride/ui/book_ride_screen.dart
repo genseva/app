1a72d385f5bd17d467bca082fc1a6c5ada7cb310
bhawyaahuja <bhawyahuja@gmail.com>
2024-05-20 10:14:46 +0530

    update: bike screen

--- lib/features/ride/ui/book_ride_screen.dart
+++ lib/features/ride/ui/book_ride_screen.dart
@@ -1,4 +1,6 @@
 import 'package:deligo/components/custom_app_bar.dart';
+import 'package:deligo/components/custom_divider.dart';
+import 'package:deligo/components/custom_field.dart';
 import 'package:deligo/generated/assets.dart';
 import 'package:deligo/generated/l10n.dart';
 import 'package:flutter/material.dart';
@@ -15,12 +17,27 @@ class BookRideScreen extends StatelessWidget {
         child: CustomAppBar(
           title: locale.whereToGo,
           image: Assets.headerHeaderRide,
-          appbarBottom: Positioned(
+          appbarBottom: PositionedDirectional(
             bottom: 0,
-            left: 8,
-            right: 0,
+            start: 10,
+            end: 10,
             child: SizedBox(
               height: 100,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.grey),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
         ),
