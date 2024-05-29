dad7eea6a206abefa64d05ad1dcf5254d5693e8f
bhawyaahuja <bhawyahuja@gmail.com>
2024-05-29 23:23:57 +0530

    add ride domain

---
+++ lib/features/common/model/ride_domain.dart
@@ -0,0 +1,9 @@
+class RideDomain {
+  final String image;
+  final String name;
+  final String time;
+  final String quantity;
+  final String price;
+
+ const RideDomain(this.image, this.name, this.time, this.quantity, this.price);
+}
